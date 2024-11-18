a391b954fb5b57802bd81ed0ec05ed0920d4283c
reon <reonsamuel2001@gmail.com>
2024-11-17 23:52:47 -0400

    Added basic HTML, CSS, and AJAX functionality

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById("searchBtn").addEventListener("click", () => {
+    fetch('superheroes.php')
+        .then(response => response.text())
+        .then(data => alert(data))
+        .catch(error => console.error('Error:', error));
+});
